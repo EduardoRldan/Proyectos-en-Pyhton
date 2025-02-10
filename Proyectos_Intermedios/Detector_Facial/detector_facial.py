ec9cf7a8abb885422236517940cba7dce838f8a2
EduardoRldan <163453549+EduardoRldan@users.noreply.github.com>
2025-02-10 14:32:03 -0300

    Detector Facial Corregido

--- Proyectos_Intermedios/Detector_Facial/detector_facial.py
+++ Proyectos_Intermedios/Detector_Facial/detector_facial.py
@@ -1,30 +1,36 @@
 import cv2
 
-# Cargar el clasificador de rostros de OpenCV (Haar Cascade)
 cascade_rostro = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-# Iniciar la cámara
 camara = cv2.VideoCapture(0)
 
+if not camara.isOpened():
+    print("Error: No se puede acceder a la cámara. ")
+    exit()
+
 while True:
-    # Capturar frame de la cámara
-    _, frame = camara.read()
-    gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convertir a escala de grises
-    
-    # Detectar rostros en la imagen
+    ret, frame = camara.read()
+    if not ret:
+        print("Error al momento de la captura del frame. ")
+        break
+
+
+    gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convertidor a escala de grises
+
+    # Detector de rostros
     rostros = cascade_rostro.detectMultiScale(gris, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # Dibujar rectángulos alrededor de los rostros detectados
-    for (x, y, w, h) in rostros:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # Dibujado del rectangulo 
+    for (x,y,w,h) in rostros:
+        cv2.rectangle(frame, (x ,y), (x + w, y + h), (0 , 255, 0,2))
 
-    # Mostrar el video en una ventana
-    cv2.imshow("Reconocimiento Facial", frame)
+    # Mostrar el video en pantalla
+    cv2.imshow("Detector Facial", frame)
 
-    # Presionar "q" para salir
+    # Presionar la letra q para salir del programa 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
-# Liberar la cámara y cerrar ventanas
+# Liberar la cámara y cierre del programa
 camara.release()
 cv2.destroyAllWindows()
